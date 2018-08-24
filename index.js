5afdf99be09b1894a3d98a3967363e45baa380de
Suheyl Turac <skbsturac@live.com>
2018-08-24 22:33:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+function add(a,b) {
+ return (a+b)
+}
 
-add (a,b)
-a + b
-a - b
-a * b
-a / b
+function subtract (a,b) {
+  return (a-b)
+}
+
+function multiply(a,b) {
+ return (a*b)
+}
+
+function divide(a,b) {
+ return (a/b)
+}
+
+function inc(a) {
+ return (++a)
+}
+
+function dec(a) {
+ return (--a)
+}
+
+function  makeInt(n) {
+return (parseInt ('n',100))
+}
